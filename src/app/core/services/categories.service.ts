6dc78cbd255eaf5eb57dcad4362c0d899732a153
buckapi <buckapis@gmail.com>
2023-06-26 08:45:01 -0500

    first commit

---
+++ src/app/core/services/categories.service.ts
@@ -0,0 +1,48 @@
+import {Category} from '@interfaces/category';
+
+export const CATEGORIES: Category[] = [
+  {
+    name:'accesorios',
+    idCategory:'ca000001'
+  }, 
+  {
+    name:'amperimetro ',
+    idCategory:'ca000002'
+  },
+  {
+    name:'arranque',
+    idCategory:'ca000003'
+  },
+  {
+    name:'bobinas',
+    idCategory:'ca000004'
+  },
+  {
+    name:'bombas',
+    idCategory:'ca000005'
+  },
+  {
+    name:'filtros',
+    idCategory:'ca000006'
+  },  
+  {
+    name:'modulos',
+    idCategory:'ca000007'
+  },  
+  {
+    name:'rep motor',
+    idCategory:'ca000008'
+  },  
+  {
+    name:'partes electricas',
+    idCategory:'ca000009'
+  },  
+  {
+    name:'valvulas', 
+    idCategory:'ca000010'
+  },  
+  {
+    name:'repuestos', 
+    idCategory:'ca000011'
+  }
+];
---
+++ src/app/core/services/vehicles.service.ts
@@ -0,0 +1,49 @@
+import {Vehicle} from '@interfaces/vehicle';
+
+export const VEHICLES: Vehicle[] = [
+  {
+    name:'Accesorios',
+    
+    idVehicle:'ca000001'
+  }, 
+  {
+    name:'Amperimetro ',
+    idVehicle:'ca000002'
+  },
+    {
+    name:'Arranques',
+    idVehicle:'ca000003'
+  },
+   {
+    name:'Bobinas',
+    idVehicle:'ca000004'
+  },
+   {
+    name:'Bombas',
+    idVehicle:'ca000005'
+  },
+   {
+    name:'Filtros',
+    idVehicle:'ca000006'
+  },  
+   {
+    name:'Modulos',
+    idVehicle:'ca000007'
+  },  
+   {
+    name:'Rep motor',
+    idVehicle:'ca000008'
+  },  
+   {
+    name:'Partes electricas',
+    idVehicle:'ca000009'
+  },  
+   {
+    name:'Valvulas',  
+    idVehicle:'ca000010'
+  },
+  {
+    name:'Repuestos',  
+    idVehicle:'ca000011'
+  }
+];
